cdbc31c9918018cbe1be868aff1c459084a5914c
Yash Bavadiya <krbavadiya11@gmail.com>
2026-08-14 22:14:28 +0530

    loop

--- shell.c
+++ shell.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
 	char *input = NULL;
 	size_t len = 0;
-	getline(&input, &len, stdin);
+	while(getline(&input, &len, stdin) != -1){
+
 	puts(input);
+	}
+	free(input);
 	return 0;
 }
