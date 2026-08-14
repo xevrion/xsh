fc9412ec26676f2535511f2ac07f86581117af33
Yash Bavadiya <krbavadiya11@gmail.com>
2026-08-14 22:39:15 +0530

    added single command support through exec and fork

--- shell.c
+++ shell.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
 int main(){
 	char *input = NULL;
 	size_t len = 0;
 	while(getline(&input, &len, stdin) != -1){
-		system(input);
+		input[strcspn(input, "\n")] = '\0';
+		// system(input);
+
+		pid_t pid = fork();
+
+		if(pid==0){
+			// child
+			execlp(input, input, NULL);
+		} else {
+			// parent
+			wait(NULL);
+		}
 	}
 	free(input);
 	return 0;
